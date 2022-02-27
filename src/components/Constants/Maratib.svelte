f837f5c2933f575c5b366c1f50eea36166a4ad81
Naved Islam <navedcoded@gmail.com>
2022-02-27 15:00:51 +0300

    redo to highlight match and table

--- src/components/Constants/Maratib.svelte
+++ src/components/Constants/Maratib.svelte
@@ -1,33 +1,93 @@
+<script>
+	import { totalNafasi } from '../../stores/totalNafasi.js';
+	import { totalSmallValue } from '../../stores/totalSmallValue.js';
+	import { totalTrueValue } from '../../stores/totalTrueValue.js';
+	import { totalMaghribiSmallValue } from '../../stores/totalMaghribiSmallValue.js';
+	import { totalMaghribiTrueValue } from '../../stores/totalMaghribiTrueValue.js';
+
+	import convertToArabicNumbers from '../Functions/convertToArabic.js'
+
+	let maratib = [
+		{
+			number: '1',
+			rank: ' الأحدية'
+		},
+		{
+			number: '2',
+			rank: 'الحقيقة المحمدية (الوحدة)'
+		},
+		{
+			number: '3',
+			rank: 'الواحدية (تجلي الأسماء والصفات)، وفيها: الكثرة، الخلق، الفتح'
+		},
+		{
+			number: '4',
+			rank: 'النبوة'
+		},
+		{
+			number: '5',
+			rank: 'الرسالة (يتفرع منها النصر)'
+		},
+		{
+			number: '6',
+			rank: 'الجمع أوالقرآن'
+		},
+		{
+			number: '7',
+			rank: 'الغنى'
+		},
+		{
+			number: '8',
+			rank: 'إحكام الدنيا (كمال الاستقرار والثبات) والسورة الثامنة الأنفال'
+		},
+		{
+			number: '9',
+			rank: 'الملك'
+		},
+		{
+			number: '10',
+			rank: 'الولاية (يتفرع منها البشرى)'
+		},
+		{
+			number: '11',
+			rank: 'الخلق الجديد'
+		},
+		{
+			number: '12',
+			rank: 'الكمال'
+		}
+	];
+</script>
+
 <div>
-    <h4>مراتب الوجود</h4>
-<ol>
-	<li>الأحدية</li>
-    <li>الحقيقة المحمدية (الوحدة)</li>
-	<li>الواحدية (تجلي الأسماء والصفات)، وفيها: الكثرة، الخلق، الفتح</li>
-	<li>النبوة</li>
-	<li>الرسالة (يتفرع منها النصر)</li>
-	<li>الجمع أوالقرآن</li>
-	<li>الغنى</li>
-	<li>إحكام الدنيا (كمال الاستقرار والثبات) والسورة الثامنة الأنفال</li>
-	<li>الملك</li>
-	<li>الولاية (يتفرع منها البشرى)</li>
-    <li>الخلق الجديد</li>
-	<li>الكمال</li>
-</ol>
+	<h4>مراتب الوجود</h4>
+	<table>
+		{#each maratib as martaba}
+			<tr>
+				{#if parseInt(martaba.number) == parseInt($totalNafasi[0]) || parseInt(martaba.number) == parseInt($totalSmallValue[0]) || parseInt(martaba.number) == parseInt($totalTrueValue[0]) || parseInt(martaba.number) == parseInt($totalMaghribiSmallValue[0]) || parseInt(martaba.number) == parseInt($totalMaghribiTrueValue[0])}
+					<td style="font-weight: bold; background-color: #fcd9d7;"
+						>{convertToArabicNumbers(martaba.number)}</td
+					>
+				{:else}
+					<td>{convertToArabicNumbers(martaba.number)}</td>
+				{/if}
+				<td>{martaba.rank}</td>
+
+			</tr>
+		{/each}
+	</table>
 </div>
 
-
 <style>
-    ol {
-        list-style: arabic-indic;
-        margin-right: 20px;
-        text-align: right;
-    }
-    li {
-        margin: 3px;
-    }
-    div {
+	div {
 		max-height: 400px;
 		overflow: auto;
+	}
+	td {
+		text-align: right;
+		padding: 2px;
+	}
+	table {
+		border-collapse: collapse;
 	}
 </style>
