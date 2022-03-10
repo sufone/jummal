c58a37818e8053c4098de0722feb643810e357d5
Naved Islam <navedcoded@gmail.com>
2022-03-10 22:25:21 +0300

    filter dictionary to only show matches

--- src/components/Constants/Dictionary.svelte
+++ src/components/Constants/Dictionary.svelte
@@ -73,50 +73,52 @@
 			<td>الجمل: غص</td>
 			<td>الجمل: غك</td>
 		</tr>
-		{#each words as word}
-			<tr>
-				<td>{word.verb}</td>
+		{#each words as word} <!--the monster below is a combo of all below filters. the goal: any match found-->
+			{#if parseInt(word.jummalNafasi) == parseInt($totalNafasi[0]) || parseInt(word.jummalNafasi) == parseInt($totalSmallValue[0]) || parseInt(word.jummalNafasi) == parseInt($totalTrueValue[0]) || parseInt(word.jummalNafasi) == parseInt($totalMaghribiSmallValue[0]) || parseInt(word.jummalNafasi) == parseInt($totalMaghribiTrueValue[0]) || parseInt(word.jummalSmall) == parseInt($totalNafasi[0]) || parseInt(word.jummalSmall) == parseInt($totalSmallValue[0]) || parseInt(word.jummalSmall) == parseInt($totalTrueValue[0]) || parseInt(word.jummalSmall) == parseInt($totalMaghribiSmallValue[0]) || parseInt(word.jummalSmall) == parseInt($totalMaghribiTrueValue[0]) || parseInt(word.jummalTrue) == parseInt($totalNafasi[0]) || parseInt(word.jummalTrue) == parseInt($totalSmallValue[0]) || parseInt(word.jummalTrue) == parseInt($totalTrueValue[0]) || parseInt(word.jummalTrue) == parseInt($totalMaghribiSmallValue[0]) || parseInt(word.jummalTrue) == parseInt($totalMaghribiTrueValue[0]) || parseInt(word.jummalMaghribiSmall) == parseInt($totalNafasi[0]) || parseInt(word.jummalMaghribiSmall) == parseInt($totalSmallValue[0]) || parseInt(word.jummalMaghribiSmall) == parseInt($totalTrueValue[0]) || parseInt(word.jummalMaghribiSmall) == parseInt($totalMaghribiSmallValue[0]) || parseInt(word.jummalMaghribiSmall) == parseInt($totalMaghribiTrueValue[0]) || parseInt(word.jummalMaghribiTrue) == parseInt($totalNafasi[0]) || parseInt(word.jummalMaghribiTrue) == parseInt($totalSmallValue[0]) || parseInt(word.jummalMaghribiTrue) == parseInt($totalTrueValue[0]) || parseInt(word.jummalMaghribiTrue) == parseInt($totalMaghribiSmallValue[0]) || parseInt(word.jummalMaghribiTrue) == parseInt($totalMaghribiTrueValue[0])}
+				<tr>
+					<td>{word.verb}</td>
 
-				{#if parseInt(word.jummalNafasi) == parseInt($totalNafasi[0]) || parseInt(word.jummalNafasi) == parseInt($totalSmallValue[0]) || parseInt(word.jummalNafasi) == parseInt($totalTrueValue[0]) || parseInt(word.jummalNafasi) == parseInt($totalMaghribiSmallValue[0]) || parseInt(word.jummalNafasi) == parseInt($totalMaghribiTrueValue[0])}
-					<td style="font-weight: bold; background-color: #fcd9d7;"
-						>{convertToArabicNumbers(word.jummalNafasi)}</td
-					>
-				{:else}
-					<td>{convertToArabicNumbers(word.jummalNafasi)}</td>
-				{/if}
+					{#if parseInt(word.jummalNafasi) == parseInt($totalNafasi[0]) || parseInt(word.jummalNafasi) == parseInt($totalSmallValue[0]) || parseInt(word.jummalNafasi) == parseInt($totalTrueValue[0]) || parseInt(word.jummalNafasi) == parseInt($totalMaghribiSmallValue[0]) || parseInt(word.jummalNafasi) == parseInt($totalMaghribiTrueValue[0])}
+						<td style="font-weight: bold; background-color: #fcd9d7;"
+							>{convertToArabicNumbers(word.jummalNafasi)}</td
+						>
+					{:else}
+						<td>{convertToArabicNumbers(word.jummalNafasi)}</td>
+					{/if}
 
-				{#if parseInt(word.jummalSmall) == parseInt($totalNafasi[0]) || parseInt(word.jummalSmall) == parseInt($totalSmallValue[0]) || parseInt(word.jummalSmall) == parseInt($totalTrueValue[0]) || parseInt(word.jummalSmall) == parseInt($totalMaghribiSmallValue[0]) || parseInt(word.jummalSmall) == parseInt($totalMaghribiTrueValue[0])}
-					<td style="font-weight: bold; background-color: #fcd9d7;"
-						>{convertToArabicNumbers(word.jummalSmall)}</td
-					>
-				{:else}
-					<td>{convertToArabicNumbers(word.jummalSmall)}</td>
-				{/if}
+					{#if parseInt(word.jummalSmall) == parseInt($totalNafasi[0]) || parseInt(word.jummalSmall) == parseInt($totalSmallValue[0]) || parseInt(word.jummalSmall) == parseInt($totalTrueValue[0]) || parseInt(word.jummalSmall) == parseInt($totalMaghribiSmallValue[0]) || parseInt(word.jummalSmall) == parseInt($totalMaghribiTrueValue[0])}
+						<td style="font-weight: bold; background-color: #fcd9d7;"
+							>{convertToArabicNumbers(word.jummalSmall)}</td
+						>
+					{:else}
+						<td>{convertToArabicNumbers(word.jummalSmall)}</td>
+					{/if}
 
-				{#if parseInt(word.jummalTrue) == parseInt($totalNafasi[0]) || parseInt(word.jummalTrue) == parseInt($totalSmallValue[0]) || parseInt(word.jummalTrue) == parseInt($totalTrueValue[0]) || parseInt(word.jummalTrue) == parseInt($totalMaghribiSmallValue[0]) || parseInt(word.jummalTrue) == parseInt($totalMaghribiTrueValue[0])}
-					<td style="font-weight: bold; background-color: #fcd9d7;"
-						>{convertToArabicNumbers(word.jummalTrue)}</td
-					>
-				{:else}
-					<td>{convertToArabicNumbers(word.jummalTrue)}</td>
-				{/if}
+					{#if parseInt(word.jummalTrue) == parseInt($totalNafasi[0]) || parseInt(word.jummalTrue) == parseInt($totalSmallValue[0]) || parseInt(word.jummalTrue) == parseInt($totalTrueValue[0]) || parseInt(word.jummalTrue) == parseInt($totalMaghribiSmallValue[0]) || parseInt(word.jummalTrue) == parseInt($totalMaghribiTrueValue[0])}
+						<td style="font-weight: bold; background-color: #fcd9d7;"
+							>{convertToArabicNumbers(word.jummalTrue)}</td
+						>
+					{:else}
+						<td>{convertToArabicNumbers(word.jummalTrue)}</td>
+					{/if}
 
-				{#if parseInt(word.jummalMaghribiSmall) == parseInt($totalNafasi[0]) || parseInt(word.jummalMaghribiSmall) == parseInt($totalSmallValue[0]) || parseInt(word.jummalMaghribiSmall) == parseInt($totalTrueValue[0]) || parseInt(word.jummalMaghribiSmall) == parseInt($totalMaghribiSmallValue[0]) || parseInt(word.jummalMaghribiSmall) == parseInt($totalMaghribiTrueValue[0])}
-					<td style="font-weight: bold; background-color: #fcd9d7;"
-						>{convertToArabicNumbers(word.jummalMaghribiSmall)}</td
-					>
-				{:else}
-					<td>{convertToArabicNumbers(word.jummalMaghribiSmall)}</td>
-				{/if}
+					{#if parseInt(word.jummalMaghribiSmall) == parseInt($totalNafasi[0]) || parseInt(word.jummalMaghribiSmall) == parseInt($totalSmallValue[0]) || parseInt(word.jummalMaghribiSmall) == parseInt($totalTrueValue[0]) || parseInt(word.jummalMaghribiSmall) == parseInt($totalMaghribiSmallValue[0]) || parseInt(word.jummalMaghribiSmall) == parseInt($totalMaghribiTrueValue[0])}
+						<td style="font-weight: bold; background-color: #fcd9d7;"
+							>{convertToArabicNumbers(word.jummalMaghribiSmall)}</td
+						>
+					{:else}
+						<td>{convertToArabicNumbers(word.jummalMaghribiSmall)}</td>
+					{/if}
 
-				{#if parseInt(word.jummalMaghribiTrue) == parseInt($totalNafasi[0]) || parseInt(word.jummalMaghribiTrue) == parseInt($totalSmallValue[0]) || parseInt(word.jummalMaghribiTrue) == parseInt($totalTrueValue[0]) || parseInt(word.jummalMaghribiTrue) == parseInt($totalMaghribiSmallValue[0]) || parseInt(word.jummalMaghribiTrue) == parseInt($totalMaghribiTrueValue[0])}
-					<td style="font-weight: bold; background-color: #fcd9d7;"
-						>{convertToArabicNumbers(word.jummalMaghribiTrue)}</td
-					>
-				{:else}
-					<td>{convertToArabicNumbers(word.jummalMaghribiTrue)}</td>
-				{/if}
-			</tr>
+					{#if parseInt(word.jummalMaghribiTrue) == parseInt($totalNafasi[0]) || parseInt(word.jummalMaghribiTrue) == parseInt($totalSmallValue[0]) || parseInt(word.jummalMaghribiTrue) == parseInt($totalTrueValue[0]) || parseInt(word.jummalMaghribiTrue) == parseInt($totalMaghribiSmallValue[0]) || parseInt(word.jummalMaghribiTrue) == parseInt($totalMaghribiTrueValue[0])}
+						<td style="font-weight: bold; background-color: #fcd9d7;"
+							>{convertToArabicNumbers(word.jummalMaghribiTrue)}</td
+						>
+					{:else}
+						<td>{convertToArabicNumbers(word.jummalMaghribiTrue)}</td>
+					{/if}
+				</tr>
+			{/if}
 		{/each}
 	</table>
 </div>
