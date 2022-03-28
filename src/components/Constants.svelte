5b928982b129c4c0fb237e482112ba6526cebb55
Naved Islam <navedcoded@gmail.com>
2022-03-28 16:18:05 +0300

    basic ayah search

--- src/components/Constants.svelte
+++ src/components/Constants.svelte
@@ -6,18 +6,19 @@
 	import { Tabs, Tab, TabContent } from 'carbon-components-svelte';
 
 	import Surahs from './Constants/Surahs.svelte';
+	import AyahSearch from './Constants/AyahSearch.svelte';
 	import Ayat from './Constants/Ayat.svelte';
 	import Asma from './Constants/Asma.svelte';
 	import Maratib from './Constants/Maratib.svelte';
 	import Dictionary from './Constants/Dictionary.svelte';
 	import { letterInput } from '../stores/letterInput.js';
-
 </script>
 
 <div>
 	<Tabs autoWidth="true">
 		<Tab label="السور" />
 		<Tab label="الآيات" />
+		<Tab label="بحث الآيات" />
 		<Tab label="الأسماء الحسنى" />
 		<Tab label="مراتب الوجود" />
 		<Tab label="المعجم" />
@@ -25,6 +26,7 @@
 		<svelte:fragment slot="content">
 			<TabContent><Surahs /></TabContent>
 			<TabContent><Ayat /></TabContent>
+			<TabContent><AyahSearch /></TabContent>
 			<TabContent><Asma /></TabContent>
 			<TabContent><Maratib /></TabContent>
 				<TabContent><Dictionary /></TabContent>
---
+++ src/components/Constants/AyahSearch.svelte
@@ -0,0 +1,24 @@
+<script>
+	import ayat from '../Functions/ayat';
+	import convertToArabicNumbers from '../Functions/convertToArabic.js';
+
+    let searchFilter = "الله"
+</script>
+
+<div>
+	<br />
+	<h4>آيات</h4>
+<textarea bind:value={searchFilter} />
+ <ol>
+     {#each ayat.filter(ayah => ayah.text.replace(/[\sًٌٍَُِّْ]+/g, '').includes(searchFilter)) as ayah, i}
+        <li>﴿{ayah.text}﴾ [{ayah.surahName}: {convertToArabicNumbers(ayah.ayahNumber)}]</li>
+    {/each}
+ </ol>   
+    </div>
+ 
+
+    <style>
+        ol {
+            text-align: right;
+        }
+    </style>
