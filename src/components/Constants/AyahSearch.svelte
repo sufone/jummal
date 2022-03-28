b40e44e72fb807e283b3554c0c176caffa29a04f
Naved Islam <navedcoded@gmail.com>
2022-03-28 16:35:56 +0300

    improve style and text filtering

--- src/components/Constants/AyahSearch.svelte
+++ src/components/Constants/AyahSearch.svelte
@@ -2,23 +2,47 @@
 	import ayat from '../Functions/ayat';
 	import convertToArabicNumbers from '../Functions/convertToArabic.js';
 
-    let searchFilter = "الله"
+	let searchFilter = 'فرط';
+    let showAyahReference = true
 </script>
 
 <div>
 	<br />
 	<h4>آيات</h4>
-<textarea bind:value={searchFilter} />
- <ol>
-     {#each ayat.filter(ayah => ayah.text.replace(/[\sًٌٍَُِّْ]+/g, '').includes(searchFilter)) as ayah, i}
-        <li>﴿{ayah.text}﴾ [{ayah.surahName}: {convertToArabicNumbers(ayah.ayahNumber)}]</li>
-    {/each}
- </ol>   
-    </div>
- 
+    <div class="top-box-container">
+        <textarea bind:value={searchFilter} />
+        <label>
+            <input type=checkbox bind:checked={showAyahReference}>
+            عمض اسم السورة
+        </label>
 
-    <style>
-        ol {
-            text-align: right;
-        }
-    </style>
+    </div>
+	<ol>
+		{#each ayat.filter((ayah) => ayah.text
+				.replace(/[ًٌٍَُِّْ]+/g, '')
+				.includes(searchFilter.replace(/[ًٌٍَُِّْ]+/g, ''))) as ayah, i}
+			<li>﴿{ayah.text}﴾ [{ayah.surahName}: {convertToArabicNumbers(ayah.ayahNumber)}]</li>
+		{/each}
+	</ol>
+</div>
+
+<style>
+	ol {
+		text-align: right;
+        list-style: arabic-indic;
+	}
+	div {
+		max-height: 600px;
+		overflow: auto;
+	}
+    div.top-box-container {
+        display: flex;
+        flex-direction: row;
+        gap: 10px;
+    }
+	@media (max-width: 600px) {
+		div {
+			margin-bottom: 120px;
+		}
+	}
+</style>
