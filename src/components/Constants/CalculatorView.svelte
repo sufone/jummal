25c6fbfd517da631fe403fbae939777be1cbf455
Naved Islam <navedcoded@gmail.com>
2022-04-06 00:39:09 +0300

    remove redundant styles

--- src/components/Constants/CalculatorView.svelte
+++ src/components/Constants/CalculatorView.svelte
@@ -13,43 +13,7 @@
 		max-height: 600px;
 		overflow: auto;
 	}
-	td,
-	th {
-		text-align: right;
-		padding: 2px;
-		border: 1px solid #999;
-	}
-	table {
-		border-collapse: collapse;
-	}
-	th {
-		background: rgb(240, 255, 249);
-		position: sticky;
-		top: 0;
-		box-shadow: 0 2px 2px -1px rgba(0, 0, 0, 0.4);
-		padding-bottom: 7px;
-	}
-	td.highlight {
-		font-weight: bold;
-		background-color: rgb(181, 255, 224);
-	}
 	:global(input, textarea, button, select, a, div) {
 		-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-	}
-	@media (prefers-color-scheme: dark) {
-		td.highlight {
-			background-color: rgb(50, 95, 77);
-			color: white;
-		}
-		th {
-			background: rgb(31, 46, 40);
-		}
-		td,
-		th {
-			border: 1px solid rgb(70, 70, 70);
-		}
-		a {
-			color: white;
-		}
 	}
 </style>
